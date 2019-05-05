97c87de0d71501ba855b027fc3c6b84ed818ab0b
youssef hoummad <youssef.hoummad@outlook.com>
2019-05-05 00:49:32 +0000

    js in

--- src/static/main.js
+++ src/static/main.js
@@ -1,3 +1,60 @@
+document.getElementById("comment").focus();
 
 
 
+// var map;
+// function initMap() {
+//  map = new google.maps.Map(document.getElementById('map'), {
+//   center: {lat: -34.397, lng: 150.644},
+//   zoom: 8,
+//   disableDefaultUI: true,
+//   draggable: false
+//  });
+// }
+
+
+
+
+
+
+
+// function to display msg in bottom
+function gotoBottom(id){
+   var element = document.getElementById(id);
+   element.scrollTop = element.scrollHeight - element.clientHeight;
+}
+
+
+// display message
+function displayMessage(content, type){
+  var classEl = "justify-content-end";
+  if (type === "receiver") {
+    classEl =   "justify-content-start";
+  };
+
+  var conversationEl = document.getElementById("conversation");
+  var messageEl = '<div class="message-body row \
+                    '+ classEl + '\
+                    ">\
+                    <div class="message-main">\
+                      <div class="\
+                      '+type+'\
+                      ">\
+                         ' + content + '\
+                      </div>\
+                    </div>\
+                  </div>';
+  conversationEl.insertAdjacentHTML('beforeend', messageEl);
+
+    // clear input
+  if (type === "receiver") {
+    document.getElementById("comment").value = "";
+  };
+}
+
+
+function replay(){
+  var text = document.getElementById("comment").value;
+  displayMessage(text, "sender");
+  displayMessage("ok", "receiver");
+}
