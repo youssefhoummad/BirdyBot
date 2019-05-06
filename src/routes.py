c514a0c9eb8539fea612acda35d7f37f168fc162
youssefhoummad <youssef.hoummad@outlook.com>
2019-05-06 19:29:29 +0000

    fix: return to old version

--- src/routes.py
+++ src/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, jsonify
 
 from src import app
 
@@ -6,3 +6,16 @@ from src import app
 @app.route("/")
 def index():
     return render_template('home.html')
+
+
+@app.route("/result", methods=['GET', 'POST'])
+def result():
+    print("start api")
+    data = {
+        "data": [{
+            "city": "elkelaa",
+            "lat": 32.0481,
+            "lng": -7.4081
+        }]
+    }
+    return jsonify(data)
